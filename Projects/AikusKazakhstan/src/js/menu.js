42fd4f12585b051f69e19293b83aa0ee24184c30
Detrimon <detrimon@gmail.com>
2022-05-13 10:58:56 +0300

    changes in pdf file names + keywords + description

---
+++ Projects/AikusKazakhstan/src/js/menu.js
@@ -0,0 +1,30 @@
+const popupMenuElement = document.querySelector(".menu-substrate");
+const bodyElement = document.body;
+
+const burgerContainerElement = document.querySelector(".menu__btn");
+burgerContainerElement.addEventListener("click", burgerOnClickHandler);
+
+const closeIconElement = document.querySelector(".menu-substrate__icon-close");
+closeIconElement.addEventListener("click", closeMenuHandler);
+
+const menuItemsElement = document.querySelector("#idPopupMenu");
+menuItemsElement.addEventListener("click", closeMenuHandler);
+
+// Event Handlers ::
+
+function burgerOnClickHandler(e) {
+  e.preventDefault();
+
+  popupMenuElement.classList.toggle("open");
+  setTimeout(function () {
+    bodyElement.style.overflowY = "hidden";
+  }, 230);
+}
+
+function closeMenuHandler(e) {
+  if (document.documentElement.scrollWidth > 960.98) return;
+  if (!e.target.dataset.hasOwnProperty("toclosemenu")) return;
+
+  bodyElement.style.overflowY = "auto";
+  popupMenuElement.classList.toggle("open");
+}
